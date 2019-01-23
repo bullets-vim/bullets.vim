d7ee68c8dafeb1b72a3fd6b2e52fe9a3e5807e57
Chayoung You <yousbe@gmail.com>
2019-01-23 15:32:24 +0900

    Support lowercase roman bullets

--- spec/bullets_spec.rb
+++ spec/bullets_spec.rb
@@ -166,6 +166,38 @@ RSpec.describe 'Bullets.vim' do
         TEXT
       end
 
+      it 'adds a new lowercase roman numeral bullet' do
+        filename = "#{SecureRandom.hex(6)}.txt"
+        write_file(filename, <<-TEXT)
+          # Hello there
+          i. this is the first bullet
+        TEXT
+
+        vim.command 'let g:bullets_pad_right = 0'
+        vim.edit filename
+        vim.type 'GA'
+        vim.feedkeys '\<cr>'
+        vim.type 'second bullet'
+        vim.feedkeys '\<cr>'
+        vim.type 'third bullet'
+        vim.feedkeys '\<cr>'
+        vim.type 'fourth bullet'
+        vim.feedkeys '\<cr>'
+        vim.type 'fifth bullet'
+        vim.write
+
+        file_contents = IO.read(filename)
+
+        expect(file_contents).to eq normalize_string_indent(<<-TEXT)
+          # Hello there
+          i. this is the first bullet
+          ii. second bullet
+          iii. third bullet
+          iv. fourth bullet
+          v. fifth bullet\n
+        TEXT
+      end
+
       it 'does not confuse with the "ignorecase" option' do
         vim.command 'set ignorecase'
         test_bullet_inserted('second line', <<-INIT, <<-EXPECTED)
